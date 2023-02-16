59f97facb590a18c2901cccc6e04acd84090cd4d
Kevin Sencherey <ksscholastic@gmail.com>
2023-02-16 01:30:45 +0000

    Favicon and title change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="w-full p-4 bg-[#08192f] md:p-8 lg:p-10 dark:bg-gray-800 h-2">
+    <footer className="w-full h-30 p-4 bg-[#08192f] md:p-8 lg:p-10 sm:h-1 dark:bg-gray-800 sm:pt-20">
       <div className="mx-auto max-w-screen-xl text-center">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400 p-10">
           © 2022-2023 Designed By &nbsp;
           <a href="#" className="hover:underline">
             Kevin Sencherey
